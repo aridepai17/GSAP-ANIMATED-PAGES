1fff9c8b4ff4bba790398bf50c205a5af8aa9ae6
aridepai17 <advaithdepai26@gmail.com>
2025-07-04 22:25:54 +0530

    feat(3dscroll): Add simple 3D Scroll page using ScrollTrigger

---
+++ 3dscroll/script.js
@@ -0,0 +1,95 @@
+gsap.registerPlugin(ScrollTrigger);
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".imageBoard",
+      pin: true,
+      start: "top top",
+      end: "+=300%",
+      scrub: 1,
+    },
+    defaults: {
+      ease: "none",
+    },
+  })
+  .to(
+    document.body,
+    {
+      delay: 0.3,
+      backgroundColor: "#f0f0f0",
+    },
+    "start"
+  )
+  .to(
+    ".upper-container h1",
+    {
+      scale: 5,
+    },
+    "start"
+  )
+  .to(
+    ".upper-container h1",
+    {
+      opacity: 0,
+    },
+    "start"
+  )
+  .to(
+    "#scaleableImg1",
+    {
+      scale: 10,
+      x: -1500,
+    },
+    "start"
+  )
+  .to(
+    "#scaleableImg2",
+    {
+      scale: 10,
+      x: -2500,
+    },
+    "start"
+  )
+  .to(
+    "#scaleableImg3",
+    {
+      scale: 10,
+      x: -2000,
+    },
+    "start"
+  )
+  .to(
+    "#scaleableImg4",
+    {
+      scale: 10,
+      x: 3000,
+    },
+    "start"
+  )
+  .to(
+    "#scaleableImg5",
+    {
+      scale: 10,
+      x: 2000,
+    },
+    "start"
+  )
+  .to(
+    ".cardImage",
+    {
+      delay: 0.1,
+      scale: 5,
+    },
+    "start"
+  )
+  .to(
+    ".cardImage",
+    {
+      scale: 5,
+      opacity: 1,
+    },
+    "start+=0.5"
+  )
+  .to(".scaleableImg", {
+    opacity: 0,
+  });
